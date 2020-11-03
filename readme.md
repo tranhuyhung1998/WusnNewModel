272fa223f13221e6ab97e3fc72a99286bfc7cede
tranhuyhung1998 <8547787huy>
2020-11-03 14:07:20 +0700

    add outputs and readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+- Vào root project
+- Cài môi trường:
+    - Tạo virtualenv (với python 3.5 trở lên)
+    - activate virtualenv
+    - gõ lệnh: pip install -r requirements.txt
+- Chạy chương trình:
+    - gõ lệnh: bash scripts/localsearch-small-alpha.sh
--- scripts/localsearch-small-alpha.sh
+++ scripts/localsearch-small-alpha.sh
@@ -1,6 +1,13 @@
-PROCS=4
+PROCS=20
 
-python LSR.py --indir small_data --init 0 --alpha 0.0 --procs $PROCS
-python LSR.py --indir small_data --init 0 --alpha 0.25 --procs $PROCS
-python LSR.py --indir small_data --init 0 --alpha 0.75 --procs $PROCS
-python LSR.py --indir small_data --init 0 --alpha 1.0 --procs $PROCS
+#python LSR.py --indir small_data --init 0 --alpha 0.0 --procs $PROCS
+#python LSR.py --indir small_data --init 0 --alpha 0.25 --procs $PROCS
+#python LSR.py --indir small_data --init 0 --alpha 0.5 --procs $PROCS
+#python LSR.py --indir small_data --init 0 --alpha 0.75 --procs $PROCS
+#python LSR.py --indir small_data --init 0 --alpha 1.0 --procs $PROCS
+
+python LSR.py --indir medium_data --init 0 --alpha 0.0 --procs $PROCS
+#python LSR.py --indir medium_data --init 0 --alpha 0.25 --procs $PROCS
+#python LSR.py --indir medium_data --init 0 --alpha 0.5 --procs $PROCS
+#python LSR.py --indir medium_data --init 0 --alpha 0.75 --procs $PROCS
+#python LSR.py --indir medium_data --init 0 --alpha 1.0 --procs $PROCS
